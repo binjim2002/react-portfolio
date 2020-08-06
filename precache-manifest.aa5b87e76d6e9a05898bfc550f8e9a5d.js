d9fc2b68a5dbad957c7245227f82b4c3e8677b92
binjim2002 <binjim2002@yahoo.fr>
2020-08-05 21:27:40 -0400

    Updates

---
+++ precache-manifest.aa5b87e76d6e9a05898bfc550f8e9a5d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929b50ca5046bb46fc637915538068cf",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "fd8b6666d14adf04ef3a",
+    "url": "/react-portfolio/static/css/main.65692969.chunk.css"
+  },
+  {
+    "revision": "35379d397f1710e2a38a",
+    "url": "/react-portfolio/static/js/2.09b6a91f.chunk.js"
+  },
+  {
+    "revision": "3648b384e94d44c82691144a3d926d4d",
+    "url": "/react-portfolio/static/js/2.09b6a91f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8b6666d14adf04ef3a",
+    "url": "/react-portfolio/static/js/main.886c739f.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "863d32d73a39cbd799bdcbad8c5b28ab",
+    "url": "/react-portfolio/static/media/IMG_8449.863d32d7.jpg"
+  },
+  {
+    "revision": "36409d3fec7125fd2bc39aac9541262c",
+    "url": "/react-portfolio/static/media/Resume-Jimmy-Kazadi.36409d3f.pdf"
+  },
+  {
+    "revision": "ebb5ac405d9451638456d7097f48d37d",
+    "url": "/react-portfolio/static/media/burgerz-handlebars.ebb5ac40.PNG"
+  },
+  {
+    "revision": "aa4e0daa47cbade258a2e1732ce5d197",
+    "url": "/react-portfolio/static/media/code-quiz.aa4e0daa.PNG"
+  },
+  {
+    "revision": "37d5ede452a1e6b4e6c21856a0ee8f51",
+    "url": "/react-portfolio/static/media/day-planner.37d5ede4.PNG"
+  },
+  {
+    "revision": "93e89c2d20e1488ce0f4c230dffac51a",
+    "url": "/react-portfolio/static/media/employee-management-system-pix.93e89c2d.PNG"
+  },
+  {
+    "revision": "f1e6f4665db6ec40c9d54fc75f22e999",
+    "url": "/react-portfolio/static/media/goodspiritsapp.f1e6f466.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e5be31b2c1c048f76d733760c29d54d8",
+    "url": "/react-portfolio/static/media/macbook-336704_1920.e5be31b2.jpg"
+  },
+  {
+    "revision": "862a37f5089e82ddc4cd301815f2aba2",
+    "url": "/react-portfolio/static/media/note-taker.862a37f5.PNG"
+  },
+  {
+    "revision": "022c8e3e9d2c387dda07a2ff6fc34419",
+    "url": "/react-portfolio/static/media/team-profile-generator-pix.022c8e3e.PNG"
+  }
+]);
